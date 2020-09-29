c5357875f3b7c5c36b0dfc8443d06fc9f6febabb
4ik <71385925+PROGRAMMIST116@users.noreply.github.com>
2020-09-29 23:24:14 +0300

    Delete 1-2.4.py

--- 1-2.4.py
+++
@@ -1,6 +0,0 @@
-def v_sred(a=5, b=4, c=3):
-    return ((a+b+c)/3)
-print(v_sred())
-x1, x2, x3 = map(int, input('Введите три числа (пробел не забудьте): ').split( ))
-v = v_sred(x1, x2, x3)
-print(v)
