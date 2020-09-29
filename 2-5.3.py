e9c2d7f16fb8017c950e39e94ce740e01e27dd94
4ik <71385925+PROGRAMMIST116@users.noreply.github.com>
2020-09-29 23:23:18 +0300

    Delete 2-5.3.py

--- 2-5.3.py
+++
@@ -1,8 +0,0 @@
-def iseven(numb):
-    """Определяет чётное или нечётное введённое число"""
-    if numb%2 == 0:
-        print('Число чётное')
-    else:
-        print('Число нечётное')
-number = int(input('Введите целое число: '))
-iseven(number)
